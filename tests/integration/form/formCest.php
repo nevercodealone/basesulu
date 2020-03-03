56c7ab9f5bd0e1de15a040e296b5add47f61d7b0
Roland Golla <rolandgolla@gmail.com>
2020-03-03 22:14:10 +0100

    add integration tests for forms and add configuration

---
+++ tests/integration/form/formCest.php
@@ -0,0 +1,33 @@
+<?php
+namespace NCA\Tests\form;
+
+use NCA\Tests\IntegrationTester;
+use NCA\Tests\Page\Base;
+
+class formCest
+{
+
+    public function _before(IntegrationTester $I, Base $page)
+    {
+        $I->amOnPage($page::$URLFORM);
+        $I->waitForElement($page::$formEmail);
+    }
+
+    /**
+     * @group db
+     */
+    public function validFormSentInsertInDb(IntegrationTester $I, Base $page)
+    {
+        $email = 'testify' . time() . '@nevercodealone.de';
+        $I->fillField($page::$formEmail, $email);
+        $I->click($page::$formSubmit);
+        $I->waitForText($page::$formSuccessText);
+
+        $I->seeInDatabase(
+            'fo_dynamics',
+            [
+                'email' => $email
+            ]
+        );
+    }
+}
